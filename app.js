3b81dc7e0bae96f5cf70a6e659f0e34f78cb5f02
pdh-24 <muhammadpaksi.mp@gmail.com>
2024-11-28 14:03:02 +0700

    Update cuyy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js'; // Importing routes
 
 // const express = require('express');
@@ -17,12 +18,56 @@ import authRoutes from './routes/authRoutes.js'; // Importing routes
 // index.js
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+import bonjour from 'bonjour';
+const bonjourService = bonjour();
+// Publikasikan layanan HTTP
+bonjourService.publish({
+  name: 'AuthService', // Nama layanan
+  type: 'http',
+  port: PORT,
+  txt: {
+    id: 'express_server_12345', // Identitas unik
+    version: '1.0',
+    description: 'Express API for Flutter Authentication',
+  },
+});
+
+// Enable CORS for all routes
+// const allowedOrigins = [
+//   'http://localhost:', // Untuk web dari localhost
+//   'http://http://10.0.2.2:', // Ganti dengan IP komputer host Anda
+// ];
+app.use(cors({
+  // origin: (origin, callback) => {
+  //   if (!origin || allowedOrigins.some(prefix => origin.startsWith(prefix))) { // Izinkan semua origin dari localhost
+  //     callback(null, true);
+  //   } else {
+  //     // Tolak origin lain
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  origin: '*',   
+  methods: ['GET', 'POST'],           // Allowed HTTP methods
+  credentials: true                   // Allow cookies and credentials
+}));
+
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
+
+// Middleware to parse JSON requests
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 
 // Use authentication routes
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,27 +1,31 @@
 // Load environment variables from the .env file
-require('dotenv').config();
+import dotenv from 'dotenv';
 
-// routes/authRoutes.js
-// const express = require('express');
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
+// Load environment variables
+dotenv.config();
+
 import { Router } from 'express';
-import { sign, verify } from 'jsonwebtoken';
 import { compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { sql } from '@vercel/postgres';
 
+// const supabase = createClient('https://vcixgtpgalxcdxxgdxpe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjaXhndHBnYWx4Y2R4eGdkeHBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3NzQ5NTEsImV4cCI6MjA0ODM1MDk1MX0.qZa9Sxw7nBIssb1zVMEkVqcGPJ0hIBQxXlvYyZ618Uw');
+dotenv.config({ path: '.env.local' }); // Load environment variables
+
+const { sign, verify } = jwt;
 const router = Router();
 
 const users = [
   {
     id: 1,
-    username: 'user1',
+    username: 'k1',
     email: 'user1@example.com',
-    password: '$2b$10$C/dmTnH4Uaz.jA6Q1Ww5beZkEdM9t1Y3Sp0jLs8AzPIpLgH8A6/eS' // bcrypt hash for 'password123'
+    password: '$2a$12$zPlq1NLDZmgofBpy6LwiB.2q5zSU5TGQdk3Fh3Ll.PFYc7mrQXaAa' // bcrypt hash for 'ti3a'
   }
 ];
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
-
+// const SECRET_KEY = "3938e9e111082bcba44f9d19461580d11177e5af410a485ea417103791fa7ce78656d3bd4651835cf2de75bb2a889717fd0eea7319733fc8c5ffff3f3032c655";
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
   
@@ -29,19 +33,19 @@ router.post('/login', async (req, res) => {
     const user = users.find((u) => u.username === username || u.email === username);
   
     if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: 'Username salah' });
     }
   
     // Check password
     const isPasswordValid = await compare(password, user.password);
   
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "password salah" });
     }
   
     // Create a JWT token
     const token = sign({ id: user.id, username: user.username }, SECRET_KEY, {
-      expiresIn: '1h',
+      expiresIn: '2h',
     });
   
     return res.status(200).json({ message: 'Login successful', token });
